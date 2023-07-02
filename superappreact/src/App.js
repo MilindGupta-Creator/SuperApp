afc8e24b7af77162f0d7b1c7fe078e7fd865efb7
MilindGupta-Creator <milindgupta578@gmail.com>
2023-07-02 22:52:38 +0530

    Prebrowse and movies list added

--- superappreact/src/App.js
+++ superappreact/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Genre from './Component/Genre/Genre.js';
 import Signup from './Pages/Signup';
 import { Routes, Route } from "react-router-dom";
+import PreBrowse from './Pages/prebrowse';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Signup />} />
     <Route path="/genre" element={<Genre/>} />
+    <Route path="/prebrowse" element={<PreBrowse/>} />
   </Routes>
   </>)
 }
